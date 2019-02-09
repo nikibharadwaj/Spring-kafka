23ec86f2dbbc8a997dd1b1cca51ff8c857e2fa85
Nikki <nikhilabharadwaj@Nikhilas-MacBook-Pro.local>
2019-02-09 17:54:44 -0500

    cc

---
+++ src/main/java/com/mygoal/kafka/springkaka/SpringKakaApplication.java
@@ -0,0 +1,14 @@
+package com.mygoal.kafka.springkaka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKakaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKakaApplication.class, args);
+	}
+
+}
+
